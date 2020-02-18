e218e04dd681f953d15c6a3a4f6212fb966744d4
ictrobot <ictrobot@outlook.com>
2020-02-18 01:11:19 +0000

    Print data from device

--- IoTMessage/index.js
+++ IoTMessage/index.js
@@ -2,7 +2,7 @@ module.exports = function (context, IoTHubMessages) {
     context.log(`JavaScript eventhub trigger function called for message array: ${IoTHubMessages}`);
     
     IoTHubMessages.forEach(message => {
-        context.log(`Processed message: ${message}`);
+        context.log(`Processed message: ${message.data}`);
     });
 
     context.done();
