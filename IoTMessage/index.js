d0a34bbf2bd80c9b45d252fd3712f265438bff26
ictrobot <ictrobot@outlook.com>
2020-02-18 23:21:56 +0000

    Cosmos DB

--- IoTMessage/index.js
+++ IoTMessage/index.js
@@ -1,4 +1,22 @@
-module.exports = function (context, IoTHubMessages) {
+const fieldsFunctions = {
+    "owner": (x => x),
+    "name": (x => x),
+    "timestamp": parseInt,
+    "count": parseInt,
+    "tempLPS": parseFloat,
+    "tempLSM": parseFloat,
+    "tempDHT": parseFloat,
+    "pressure": parseFloat,
+    "humidity": parseFloat,
+    "eco2": parseInt,
+    "tvoc": parseInt,
+    "devs": parseInt,
+    "bss": parseInt
+};
+
+module.exports = (context, IoTHubMessages) => {
+    let dbDocuments = []
+
     IoTHubMessages.forEach((message, i) => {
         const properties = context.bindingData.propertiesArray[i];
         const owner = properties.owner;
@@ -10,16 +28,18 @@ module.exports = function (context, IoTHubMessages) {
 
         let rows = message.data.trim().split("\n");
         let fields = rows.shift().split(",").map(x => x.trim());
-        let processed = [];
         
         rows.forEach(row => {
-            let o = {};
-            row.split(",").forEach((item, i) => o[fields[i]] = item.trim());
-            processed.push(o);
+            let o = {owner: owner, name: name};
+            row.split(",").map(x => x.trim()).forEach((item, i) => {
+                const field = fields[i];
+                o[field] = fieldsFunctions[field](item);
+            });
+            dbDocuments.push(o);
         });
-
-        context.log(JSON.stringify({properties: {owner: owner, name: name}, data: processed}));
     });
+
+    context.bindings.documents = JSON.stringify(dbDocuments);
 
     context.done();
 };
