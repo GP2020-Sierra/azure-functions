330abf24d53709cab71ccf5e8929b24bda4ca9b3
ictrobot <ictrobot@outlook.com>
2020-02-18 02:29:42 +0000

    Extract binding properties and process CSV

--- IoTMessage/index.js
+++ IoTMessage/index.js
@@ -1,8 +1,24 @@
 module.exports = function (context, IoTHubMessages) {
-    context.log(`JavaScript eventhub trigger function called for message array: ${IoTHubMessages}`);
-    
-    IoTHubMessages.forEach(message => {
-        context.log(`Processed message: ${message.data}`);
+    IoTHubMessages.forEach((message, i) => {
+        const properties = context.bindingData.propertiesArray[i];
+        const owner = properties.owner;
+        const name = properties.name;
+
+        // context.log(`Data: ${message.data}`);
+        // context.log(`Owner: ${owner}`);
+        // context.log(`Name: ${name}`);
+
+        let rows = message.data.trim().split("\n");
+        let fields = rows.shift().split(",").map(x => x.trim());
+        let processed = [];
+        
+        rows.forEach(row => {
+            let o = {};
+            row.split(",").forEach((item, i) => o[fields[i]] = item.trim());
+            processed.push(o);
+        });
+
+        context.log(JSON.stringify({properties: {owner: owner, name: name}, data: processed}));
     });
 
     context.done();
